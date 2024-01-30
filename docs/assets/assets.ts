7ff9cbbde95f1d949d68425d67d519b6cf22bb2b
Vadim Budarin <budarin-vv@yandex.ru>
2024-01-30 18:49:21 +0300

    fix

---
+++ docs/assets/assets.ts
@@ -0,0 +1,8 @@
+export const assets: string[] = [
+    '/manifest.webmanifest',
+    '/favicon.ico',
+    '/favicon-32x32.png',
+    '/android-chrome-192x192.png',
+    '/android-chrome-512x512.png',
+    '/8ad17092b67f41ed6f08.mp3',
+];
